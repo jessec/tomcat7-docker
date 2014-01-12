a9e86f12e5b5deee686a235215d6edcbdd9b1f1b
Thanneer <tm@beyondbytes.co.in>
2014-01-12 22:50:02 +0530

    Added env variable usage

--- deploy_run.sh
+++ deploy_run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-echo Starting "$1" on port "$2"
+echo Starting "$WAR_URL" on port "$PORT"
 #Copy the war file from mounted directory to tomcat webapps directory
-if [ $1 ]
+if [ $WAR_URL ]
 then
 rm -r /var/lib/tomcat7/webapps/*
-cd /var/lib/tomcat7/webapps/  && wget $1
+cd /var/lib/tomcat7/webapps/  && wget $WAR_URL
 fi
 #Add the port to the JVM args
-if [ $2 ]
+if [ $PORT ]
 then
-echo "export JAVA_OPTS=\"-Xms512m -Xmx1024m -Dport.http.nonssl=$2\"" >> /usr/share/tomcat7/bin/setenv.sh
+echo "came in .....with port $PORT"
+echo "export JAVA_OPTS=\"-Xms512m -Xmx1024m -Dport.http.nonssl=$PORT\"" >> /usr
 else
-echo "export JAVA_OPTS=\"-Xms512m -Xmx1024m -Dport.http.nonssl=8080\"" >> /usr/share/tomcat7/bin/setenv.sh
+echo "export JAVA_OPTS=\"-Xms512m -Xmx1024m -Dport.http.nonssl=8080\"" >> /usr/
 fi
 
 service tomcat7 start
