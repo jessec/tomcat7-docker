b7bdac210f6c6f45407fd361a4f360a062fabba4
Thanneer <tm@beyondbytes.co.in>
2014-01-16 13:59:20 +0530

    Update deploy_run.sh

--- deploy_run.sh
+++ deploy_run.sh
@@ -6,14 +6,7 @@ then
 rm -r /var/lib/tomcat7/webapps/*
 cd /var/lib/tomcat7/webapps/  && wget -q $WAR_URL
 fi
-#Add the port to the JVM args
-if [ $PORT ]
-then
-echo "came in .....with port $PORT"
-echo "export JAVA_OPTS=\"-Xms512m -Xmx1024m -Dport.http.nonssl=$PORT\"" >> /usr
-else
-echo "export JAVA_OPTS=\"-Xms512m -Xmx1024m -Dport.http.nonssl=8080\"" >> /usr/
-fi
+
 
 service tomcat7 start
 tail -F /var/lib/tomcat7/logs/catalina.out
